dd78620ceb3fe9dae0b5476fdedae923dd518ec3
brinkqiang <brink.qiang@gmail.com>
2024-12-29 11:27:31 +0800

    add

--- test/dmenablewarnningtest/dmdisablewarnningtest.cpp
+++ test/dmenablewarnningtest/dmdisablewarnningtest.cpp
@@ -6,10 +6,11 @@
 
 int main( int argc, char* argv[] ) {
 
+    DMLOG_INIT();
     std::vector<int> vecData;
     strtk::parse( "1,2,3,4,5,6,7,8,9", ",", vecData );
 
-
+    LOG_WARN("{}", strtk::join(vecData, ","));
 
     return 0;
 }
