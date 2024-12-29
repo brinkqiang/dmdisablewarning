f24d4cd6c1c2e6d606049693f22ef7b2e52294ab
brinkqiang <brink.qiang@gmail.com>
2024-12-29 11:24:12 +0800

    add

--- test/dmdisablewarnningtest/dmdisablewarnningtest.cpp
+++ test/dmdisablewarnningtest/dmdisablewarnningtest.cpp
@@ -1,14 +1,18 @@
 
 #include "dmdisablewarnning.h"
 
-//DISABLE_ALL_WARNINGS
+DISABLE_ALL_WARNINGS
 #include "dmstrtk.hpp"
-//ENABLE_ALL_WARNINGS
+#include "dmutil.h"
+#include "dmlog.hpp"
+ENABLE_ALL_WARNINGS
 
 int main( int argc, char* argv[] ) {
 
     std::vector<int> vecData;
     strtk::parse( "1,2,3,4,5,6,7,8,9", ",", vecData );
+
+
 
     return 0;
 }
