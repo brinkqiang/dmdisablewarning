6f0755940431a15140e075532af11c7301310993
brinkqiang <brink.qiang@gmail.com>
2025-03-15 15:47:42 +0800

    add

--- build.sh
+++ build.sh
@@ -12,12 +12,12 @@
 
 rm -rf build
 mkdir -p build
-pushd build
+cd build
 
 cmake -DCMAKE_BUILD_TYPE=relwithdebinfo ..
-cmake --build . --config relwithdebinfo -- -j$(nproc)
+cmake --build . --config relwithdebinfo
 
-popd
+cd ..
 
 # popd
 
